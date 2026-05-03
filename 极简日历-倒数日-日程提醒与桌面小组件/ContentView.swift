3acbfa362856755acb3c852a9ea164b909f71ef0
jok9580955 <jok9580955@gmail.com>
2026-05-04 01:21:55 +0800

    Initial Commit

---
+++ 极简日历-倒数日-日程提醒与桌面小组件/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  极简日历-倒数日-日程提醒与桌面小组件
+//
+//  Created by ll on 2026/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 极简日历-倒数日-日程提醒与桌面小组件/___________________App.swift
@@ -0,0 +1,17 @@
+//
+//  ___________________App.swift
+//  极简日历-倒数日-日程提醒与桌面小组件
+//
+//  Created by ll on 2026/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ___________________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
